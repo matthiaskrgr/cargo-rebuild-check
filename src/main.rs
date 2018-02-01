b8147399c09e45b42aa5866b38a8544e12cbe437
Matthias Krüger <matthias.krueger@famsik.de>
2018-02-01 12:25:40 +0100

    add checking of binaries

--- src/main.rs
+++ src/main.rs
@@ -23,15 +23,27 @@ fn main() {
     // check all files in this dir
     for binary in fs::read_dir(&bin_dir).unwrap() {
         let path = binary.unwrap();
-        let string = path.path();
-        println!("{}", &string.display());
-        match Command::new("ldd").arg(&string).output() {
+        let name_string = path.path();
+        println!("checking: {}", &name_string.display());
+        match Command::new("ldd").arg(&name_string).output() {
             Ok(out) => {
-                println!("git gc error\nstatus: {}", out.status);
-                println!("stdout:\n {}", String::from_utf8_lossy(&out.stdout));
-                println!("stderr:\n {}", String::from_utf8_lossy(&out.stderr));
+            //    println!("git gc error\nstatus: {}", out.status);
+            //    println!("stdout:\n {}", String::from_utf8_lossy(&out.stdout));
+            //    println!("stderr:\n {}", String::from_utf8_lossy(&out.stderr));
                 //if out.status.success() {}
-                let output = &out.stdout;
+                let output = String::from_utf8_lossy(&out.stdout);
+                let output = output.into_owned();
+                let mut first = true;
+                for line in output.lines() {
+                    if line.ends_with("=> not found")  {
+                            if first {
+                                println!("binary: {}", name_string.display());
+                            }
+                            println!("is missing: {}",  line);
+                            first = false;
+                     }
+                    //println!("{}", line);
+                }
             }
             Err(e) => println!("ERROR '{}'", e),
         }
