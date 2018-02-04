589e58707a644d5765ad025422ba769e232db9a9
Matthias Krüger <matthias.krueger@famsik.de>
2018-02-04 14:12:37 +0100

    move binary checking into function

--- src/main.rs
+++ src/main.rs
@@ -13,39 +13,47 @@
 
 extern crate cargo;
 
-use std::fs;
+use std::fs::*;
 use std::process::Command;
+
+fn check_file(path: &DirEntry) {
+    let mut print_string = String::new();
+    let name_string = path.path();
+    print_string.push_str(&format!("checking: {}", &name_string.display()));
+
+    match Command::new("ldd").arg(&name_string).output() {
+        Ok(out) => {
+            //    println!("git gc error\nstatus: {}", out.status);
+            //    println!("stdout:\n {}", String::from_utf8_lossy(&out.stdout));
+            //    println!("stderr:\n {}", String::from_utf8_lossy(&out.stderr));
+            //if out.status.success() {}
+            let output = String::from_utf8_lossy(&out.stdout);
+            let output = output.into_owned();
+            let mut first = true;
+            for line in output.lines() {
+                if line.ends_with("=> not found") {
+                    if first {
+                        print_string.push_str(&format!("\nbinary: {}\n", &name_string.display()));
+                    }
+                    print_string.push_str(&format!("\t\t is missing: {}\n", line.replace("=> not found", "").trim()));
+                    first = false;
+                }
+                //println!("{}", line);
+            }
+        }
+        Err(e) => panic!("ERROR '{}'", e),
+    }
+    if print_string.len() > 1 {
+        println!("{}", print_string);
+    }
+}
 
 fn main() {
     let cargo_cfg = cargo::util::config::Config::default().unwrap();
     let mut bin_dir = cargo_cfg.home().clone().into_path_unlocked();
     bin_dir.push("bin");
     // check all files in this dir
-    for binary in fs::read_dir(&bin_dir).unwrap() {
-        let path = binary.unwrap();
-        let name_string = path.path();
-        println!("checking: {}", &name_string.display());
-        match Command::new("ldd").arg(&name_string).output() {
-            Ok(out) => {
-                //    println!("git gc error\nstatus: {}", out.status);
-                //    println!("stdout:\n {}", String::from_utf8_lossy(&out.stdout));
-                //    println!("stderr:\n {}", String::from_utf8_lossy(&out.stderr));
-                //if out.status.success() {}
-                let output = String::from_utf8_lossy(&out.stdout);
-                let output = output.into_owned();
-                let mut first = true;
-                for line in output.lines() {
-                    if line.ends_with("=> not found") {
-                        if first {
-                            println!("binary: {}", name_string.display());
-                        }
-                        println!("\t\t is missing: {}", line.replace("=> not found", "").trim());
-                        first = false;
-                    }
-                    //println!("{}", line);
-                }
-            }
-            Err(e) => println!("ERROR '{}'", e),
-        }
+    for binary in read_dir(&bin_dir).unwrap() {
+        check_file(&binary.unwrap());
     }
 }
