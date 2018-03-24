1778073e8214fe3298469ca9bc51b86aa340e666
Matthias Krüger <matthias.krueger@famsik.de>
2018-03-24 21:53:24 +0100

    move more code from main() to core.rs

--- src/core.rs
+++ src/core.rs
@@ -12,10 +12,14 @@
 
 extern crate cargo;
 
+extern crate rayon;
+
 use std;
 use std::fs::File;
 use std::io::prelude::*;
 use std::process::Command;
+
+use core::rayon::iter::*;
 
 // a package that we may need to rebuild
 pub struct CrateInfo {
@@ -263,4 +267,90 @@ pub fn get_rustc_lib_path() -> String {
         .expect("Failed to convert pathBuf to String");
 
     rust_lib_path_string
+}
+
+pub fn check_and_rebuild_broken_crates(
+    packages: Vec<CrateInfo>,
+    rust_lib_path: &str,
+    bin_dir: &std::path::PathBuf,
+    do_auto_rebuild: bool,
+) {
+    // iterate (in parallel) over the acquired metadata and check for broken library links
+    // filter out all None values, only collect the Some() ones
+
+    // todo: can we avoid sorting into a separate vector here?
+    let broken_pkgs: Vec<&CrateInfo> = packages
+        .par_iter()
+        .filter_map(|binary| check_binary(binary, &bin_dir, &rust_lib_path))
+        .collect();
+
+    let rebuilds_required: bool = !broken_pkgs.is_empty();
+
+    if rebuilds_required {
+        // concat list of names of crates needing rebuilding
+        let mut pkgs_string = String::new();
+        for pkg in &broken_pkgs {
+            pkgs_string.push_str(&pkg.name);
+            pkgs_string.push_str(" ");
+        }
+        println!("\n  Crates needing rebuild: {}", pkgs_string.trim());
+        if !do_auto_rebuild {
+            std::process::exit(2);
+        }
+    } else {
+        println!("\n  Everything looks good! :)");
+    }
+    let mut list_of_failures: Vec<String> = Vec::new();
+
+    // try to rebuild broken packages
+    if rebuilds_required && do_auto_rebuild {
+        // we need to find out if a package is a git package
+        for pkg in broken_pkgs {
+            let mut cargo_args: Vec<String> = Vec::new();
+            match pkg.git {
+                Some(ref git_repo_addr) => {
+                    cargo_args.push("--git".to_string());
+                    cargo_args.push(git_repo_addr.to_string());
+                    // we have a git package, check if it has branch, tag or rev, else install from repo
+
+                    if let Some(ref branch) = pkg.branch {
+                        cargo_args.push("--branch".to_string());
+                        cargo_args.push(branch.to_string());
+                    }
+
+                    if let Some(ref tag) = pkg.tag {
+                        cargo_args.push("--tag".to_string());
+                        cargo_args.push(tag.to_string());
+                    }
+                    if let Some(ref rev) = pkg.rev {
+                        cargo_args.push("--rev".to_string());
+                        cargo_args.push(rev.to_string());
+                    }
+                } // Some(ref git_repo_addr)
+                None => {
+                    // normal crates.io package?
+                    if let Some(ref registry) = pkg.registry {
+                        if registry == "https://github.com/rust-lang/crates.io-index" {
+                            // crates io, reinstall the same version
+                            cargo_args.push("--version".to_string());
+                            cargo_args.push(pkg.version.to_string());
+                        } else {
+                            eprintln!("error unknown registry!");
+                            panic!();
+                        }
+                    } // match pkg.registry
+                      // if we just have a path, there's not much we can do I guess
+                    if let Some(ref path) = pkg.path {
+                        cargo_args.push("--path".to_string());
+                        cargo_args.push(path.to_string());
+                    } // match pkg.path
+                } // pkg.git == None /// else
+            } // match pkg.git
+
+            run_cargo_install(&pkg.name, &cargo_args, &mut list_of_failures);
+        }
+    }
+    if !list_of_failures.is_empty() {
+        println!("Failed rebuilds: {}", list_of_failures.join(" "));
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,10 @@
 extern crate cargo;
 #[macro_use]
 extern crate clap;
-extern crate rayon;
 
 mod check_external_cmds;
 mod cli;
 mod core;
-
-use rayon::prelude::*;
 
 use check_external_cmds::*;
 use cli::*;
@@ -51,83 +48,10 @@ fn main() {
     // get the path where rustc libs are stored: $(rustc --print sysroot)/lib
     let rust_lib_path = get_rustc_lib_path();
 
-    // iterate (in parallel) over the acquired metadata and check for broken library links
-    // filter out all None values, only collect the Some() ones
-
-    // todo: can we avoid sorting into a separate vector here?
-    let broken_pkgs: Vec<&CrateInfo> = packages
-        .par_iter()
-        .filter_map(|binary| check_binary(binary, &bin_dir, &rust_lib_path))
-        .collect();
-
-    let rebuilds_required: bool = !broken_pkgs.is_empty();
-    let do_auto_rebuild = cfg.is_present("auto-rebuild");
-
-    if rebuilds_required {
-        // concat list of names of crates needing rebuilding
-        let mut pkgs_string = String::new();
-        for pkg in &broken_pkgs {
-            pkgs_string.push_str(&pkg.name);
-            pkgs_string.push_str(" ");
-        }
-        println!("\n  Crates needing rebuild: {}", pkgs_string.trim());
-        if !do_auto_rebuild {
-            std::process::exit(2);
-        }
-    } else {
-        println!("\n  Everything looks good! :)");
-    }
-    let mut list_of_failures: Vec<String> = Vec::new();
-
-    // try to rebuild broken packages
-    if rebuilds_required && do_auto_rebuild {
-        // we need to find out if a package is a git package
-        for pkg in broken_pkgs {
-            let mut cargo_args: Vec<String> = Vec::new();
-            match pkg.git {
-                Some(ref git_repo_addr) => {
-                    cargo_args.push("--git".to_string());
-                    cargo_args.push(git_repo_addr.to_string());
-                    // we have a git package, check if it has branch, tag or rev, else install from repo
-
-                    if let Some(ref branch) = pkg.branch {
-                        cargo_args.push("--branch".to_string());
-                        cargo_args.push(branch.to_string());
-                    }
-
-                    if let Some(ref tag) = pkg.tag {
-                        cargo_args.push("--tag".to_string());
-                        cargo_args.push(tag.to_string());
-                    }
-                    if let Some(ref rev) = pkg.rev {
-                        cargo_args.push("--rev".to_string());
-                        cargo_args.push(rev.to_string());
-                    }
-                } // Some(ref git_repo_addr)
-                None => {
-                    // normal crates.io package?
-                    if let Some(ref registry) = pkg.registry {
-                        if registry == "https://github.com/rust-lang/crates.io-index" {
-                            // crates io, reinstall the same version
-                            cargo_args.push("--version".to_string());
-                            cargo_args.push(pkg.version.to_string());
-                        } else {
-                            eprintln!("error unknown registry!");
-                            panic!();
-                        }
-                    } // match pkg.registry
-                      // if we just have a path, there's not much we can do I guess
-                    if let Some(ref path) = pkg.path {
-                        cargo_args.push("--path".to_string());
-                        cargo_args.push(path.to_string());
-                    } // match pkg.path
-                } // pkg.git == None /// else
-            } // match pkg.git
-
-            run_cargo_install(&pkg.name, &cargo_args, &mut list_of_failures);
-        }
-    }
-    if !list_of_failures.is_empty() {
-        println!("Failed rebuilds: {}", list_of_failures.join(" "));
-    }
+    check_and_rebuild_broken_crates(
+        packages,
+        &rust_lib_path,
+        &bin_dir,
+        cfg.is_present("auto-build"),
+    )
 }
