9d7845df9555c8845716721eca0b887430e1cdd2
Matthias Krüger <matthias.krueger@famsik.de>
2018-03-26 14:07:30 +0200

    add warning boilerplate to core.rs

--- src/core.rs
+++ src/core.rs
@@ -1,3 +1,15 @@
+// enable additional rustc warnings
+#![warn(trivial_casts, trivial_numeric_casts, unsafe_code)]
+// enable additional clippy warnings
+#![cfg_attr(feature = "cargo-clippy", warn(int_plus_one))]
+#![cfg_attr(feature = "cargo-clippy", warn(shadow_reuse, shadow_same, shadow_unrelated))]
+#![cfg_attr(feature = "cargo-clippy", warn(mut_mut))]
+#![cfg_attr(feature = "cargo-clippy", warn(nonminimal_bool))]
+#![cfg_attr(feature = "cargo-clippy", warn(pub_enum_variant_names))]
+#![cfg_attr(feature = "cargo-clippy", warn(range_plus_one))]
+#![cfg_attr(feature = "cargo-clippy", warn(string_add, string_add_assign))]
+#![cfg_attr(feature = "cargo-clippy", warn(stutter))]
+
 extern crate cargo;
 extern crate rayon;
 extern crate test;
